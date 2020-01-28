543ff5273ec798f4b413b4b084d295584d8813df
soldesk <soldesk@soldesk-PC>
2020-01-28 10:34:33 +0900

    Home버튼의 index.do컨트롤러

--- src/main/java/dev/mvc/team/HomeController.java
+++ src/main/java/dev/mvc/team/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +36,18 @@ public class HomeController {
 		
 		return "home";
 	}
+	/**
+	  * 시작 페이지
+	  * @param mno
+	  * @return
+	  */
+	 @RequestMapping(value = "/index.do", 
+	                           method = RequestMethod.GET)
+	 public ModelAndView home3() {
+	   ModelAndView mav = new ModelAndView();
+	   mav.setViewName("/index"); // /webapp/index.jsp
+
+	   return mav;
+	 } 
 	
 }
