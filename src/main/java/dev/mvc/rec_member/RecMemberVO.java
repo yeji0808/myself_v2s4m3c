e52162d053572a369a13ed9a2ce3302deffe607f
soldesk <soldesk@soldesk-PC>
2020-01-31 17:53:28 +0900

    rec_member

---
+++ src/main/java/dev/mvc/rec_member/RecMemberVO.java
@@ -0,0 +1,35 @@
+package dev.mvc.rec_member;
+
+public class RecMemberVO {
+  private int RecMemberNo;
+  private String Rdate;
+  private int recom_no;
+  private int memberno;
+  
+  public int getRecMemberNo() {
+    return RecMemberNo;
+  }
+  public void setRecMemberNo(int recMemberNo) {
+    RecMemberNo = recMemberNo;
+  }
+  public String getRdate() {
+    return Rdate;
+  }
+  public void setRdate(String rdate) {
+    Rdate = rdate;
+  }
+  public int getRecom_no() {
+    return recom_no;
+  }
+  public void setRecom_no(int recom_no) {
+    this.recom_no = recom_no;
+  }
+  public int getMemberno() {
+    return memberno;
+  }
+  public void setMemberno(int memberno) {
+    this.memberno = memberno;
+  }
+  
+  
+}
