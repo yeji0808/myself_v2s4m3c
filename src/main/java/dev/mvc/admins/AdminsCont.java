696f60f366733e329c4bed5b5d1f59a5e55ba9be
soldesk <soldesk@soldesk-PC>
2020-02-03 16:13:36 +0900

    관리자

--- src/main/java/dev/mvc/admins/AdminsCont.java
+++ src/main/java/dev/mvc/admins/AdminsCont.java
@@ -52,7 +52,7 @@ public class AdminsCont {
   public ModelAndView login(HttpServletRequest request) {
     ModelAndView mav = new ModelAndView();
     
-    mav.setViewName("redirect:/admins/login_form");
+    mav.setViewName("redirect:/admins/login_form.jsp");
     return mav;
   }
   
